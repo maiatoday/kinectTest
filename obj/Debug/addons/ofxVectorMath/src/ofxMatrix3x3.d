83d8783dadd0e6d76e6faed8f3ef2e7ba2408597
maiatoday <maiatoday@gmail.com>
2011-02-04 11:25:31 +0200

    Initial commit of kinect example for Ubuntu 10.10 based on ofxKinect example

---
+++ obj/Debug/addons/ofxVectorMath/src/ofxMatrix3x3.d
@@ -0,0 +1,3 @@
+obj/Debug/addons/ofxVectorMath/src/ofxMatrix3x3.o: \
+ /home/maia/oF/of_preRelease_v0062_linux_FAT/addons/ofxVectorMath/src/ofxMatrix3x3.cpp \
+ /home/maia/oF/of_preRelease_v0062_linux_FAT/addons/ofxVectorMath/src/ofxMatrix3x3.h
